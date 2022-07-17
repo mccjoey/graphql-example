08e84eed01c8d2d915c6bbda215bed448c56ab48
José Sampaio <jose@pippa.com.br>
2022-07-16 22:08:28 -0300

    Setting up Apollo Server

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 const path = require("path");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray,
-});
+async function startApolloServer() {
+  const app = express();
 
-const root = {
-  products: require("./products/products.model"),
-  orders: require("./orders/orders.model"),
-};
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
 
-const app = express();
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+  const server = new ApolloServer({
+    schema
   })
-);
 
-app.listen(3000, () => {
-  console.log("Running GraphQL server...");
-});
+  await server.start();
+  server.applyMiddleware({app, path : "/graphql"});
+
+  app.listen(3000, () => {
+    console.log("Running GraphQL server...");
+  });
+  
+}
+
+
+startApolloServer()
+
+
